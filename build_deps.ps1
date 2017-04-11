5b44b65b3c4a6b59375eca1908d86bd13234310e
centny <centny@gmail.com>
2017-04-11 17:00:39 +0800

    fix build error.

--- build_deps.ps1
+++ build_deps.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference="Stop"
 
 cd ../boost
-# ./bootstrap
-# ./b2 -j 24 define=BOOST_USE_WINAPI_VERSION=0x0601 link=shared runtime-link=shared --prefix=..\deps --layout=system address-model=64 release install
+ ./bootstrap
+ ./b2 -j 24 define=BOOST_USE_WINAPI_VERSION=0x0601 link=shared runtime-link=shared --prefix=..\deps --layout=system address-model=64 release install
 
 cd ../libed2k
 rm -r -Force build64
@@ -12,9 +12,9 @@ cmake -G "Visual Studio 14 2015 Win64" -DBOOST_ROOT="$pwd/../../deps" -DCMAKE_IN
 msbuild INSTALL.vcxproj /p:Configuration=Release
 cd ../
 
-#cd ../openssl
-#perl Configure VC-WIN64A shared no-asm --prefix="$pwd/../../deps"
-#nmake install
+cd ../openssl
+perl Configure VC-WIN64A shared no-asm --prefix="$pwd/../deps"
+nmake install
 
 
 cd ../libemulex
@@ -24,6 +24,8 @@ cd build64
 cmake -G "Visual Studio 14 2015 Win64" -DBOOST_ROOT="$pwd/../../deps" -DCMAKE_INSTALL_PREFIX="$pwd/../../deps" ..
 msbuild INSTALL.vcxproj /p:Configuration=Release
 cd ../
+
+cp ../deps/lib/*.dll ../deps/bin/
 
 cd ../libemulex-node
 node-gyp -DDEPS_DIR="$pwd/../../deps" rebuild
